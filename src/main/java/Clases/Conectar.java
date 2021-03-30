25807aba5be7eba77077bfb3bce1997402d856f8
Luiss Montoya <luisgoemon@gmail.com>
2021-03-29 22:32:39 -0500

    Final
    Crud

---
+++ src/main/java/Clases/Conectar.java
@@ -0,0 +1,31 @@
+
+package Clases;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+    
+/**
+ *
+ * @author Luiss
+ */
+public class Conectar {
+    
+   Connection cn;
+   
+   public Connection conexion(){
+       try {
+           Class.forName("com.mysql.jdbc.Driver");
+           
+           cn = DriverManager.getConnection("jdbc:mysql://localhost/work","root","55555");
+           System.out.println("Conectado");
+       
+       } catch (Exception e) {
+           System.out.println("Error al conectar");
+           System.out.println(e.getMessage());
+       }
+   return cn;
+   }
+    
+    
+    
+}
